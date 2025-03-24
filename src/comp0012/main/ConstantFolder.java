307e52491299a0eaa00e0a870b0ba3e824dc700e
Uras <uras.asil2004@gmail.com>
2025-03-24 21:36:36 +0000

    simple folding

--- src/comp0012/main/ConstantFolder.java
+++ src/comp0012/main/ConstantFolder.java
@@ -9,6 +9,7 @@ import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.*;
 import org.apache.bcel.generic.ClassGen;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.InstructionHandle;
@@ -44,8 +45,114 @@ public class ConstantFolder
 		ConstantPoolGen cpgen = cgen.getConstantPool();
 
 		// Implement your optimization here
+		for(Method method: gen.getMethods()){
+		    MethodGen methodGen = new MethodGen(method, gen.getClassName(), cpgen);
+		    InstructionList il = methodGen.getInstructionList();
+		    if(il == null)continue;
+		    InstructionList working_list = il.copy();
+		    InstructionFinder finder = new InstructionFinder(il);
+		    String[] patterns = {
+                    "ICONST ICONST (IADD|ISUB|IMUL|IDIV)",
+                    "LCONST LCONST (LADD|LSUB|LMUL|LDIV)",
+                    "FCONST FCONST (FADD|FSUB|FMUL|FDIV)",
+                    "DCONST DCONST (DADD|DSUB|DMUL|DDIV)",
+		    "ICONST ICONST (IF_ICMPGT|IF_ICMPLT|IF_ICMPEQ)",
+                    "LCONST LCONST (LCMP)"
+                    };
+		    boolean modified = false;
+		    for(String pattern: patterns){
+		        for(Iterator<InstructionHandle[]> it = finder.search(pattern); it.hasNext();){
+			    InstructionHandle[] match = it.next();
+			    Number const1 = get_constant_value(match[0].getInstruction());
+			    Number const2 = get_constant_value(match[1].getInstruction());
+			    Instruction arithmetic_op = match[2].getInstruction();
+			    if(const1 == null || const2 == null)continue;
+			    Number result = compute_arithmetic_result(const1, const2, arithmetic_op);
+			    if(result != null){
+			        try{
+			            InstructionHandle new_instruction = il.insert(match[0], get_push_instruction(result));
+			            working_list.delete(match[0]);
+				    working_list.delete(match[1]);
+				    working_list.delete(match[2]);
+				    modified = true;
+			        }
+			        catch(TargetLostException e){
+			            e.printStackTrace();
+			        }
+			    }
+
+			    Boolean comparison_result = compute_comparison_result(const1, const2, arithmetic_op);
+			    if(comparison_result != null){
+			        try{
+				    InstructionHandle new_instruction = il.insert(match[0], new ICONST(comparison_result ? 1:0));
+				    working_list.delete(match[0]);
+				    working_list.delete(match[1]);
+				    working_list.delete(match[2]);
+				    modified = true;
+				}
+				catch(TargetLostException e){
+				    e.printStackTrace();
+				}
+			    }
+			}
+		    }
+		    if(modified){
+			methodGen.setInstructionList(working_list);
+			methodGen.setMaxStack();
+			methodGen.setMaxLocals();
+			Method optimised = methodGen.getMethod();
+			gen.replaceMethod(method, optimised);
+		    }
+
+		}
         
 		this.optimized = gen.getJavaClass();
+	}
+	private Number get_constant_value(Instruction instruction) {
+            if (instruction instanceof ConstantPushInstruction) {
+                return ((ConstantPushInstruction) instruction).getValue();
+            }
+            return null;
+        }
+
+    	private Number compute_arithmetic_result(Number val1, Number val2, Instruction op) {
+            if (op instanceof IADD) return val1.intValue() + val2.intValue();
+            if (op instanceof ISUB) return val1.intValue() - val2.intValue();
+            if (op instanceof IMUL) return val1.intValue() * val2.intValue();
+            if (op instanceof IDIV && val2.intValue() != 0) return val1.intValue() / val2.intValue();
+
+            if (op instanceof LADD) return val1.longValue() + val2.longValue();
+            if (op instanceof LSUB) return val1.longValue() - val2.longValue();
+            if (op instanceof LMUL) return val1.longValue() * val2.longValue();
+            if (op instanceof LDIV && val2.longValue() != 0) return val1.longValue() / val2.longValue();
+
+            if (op instanceof FADD) return val1.floatValue() + val2.floatValue();
+            if (op instanceof FSUB) return val1.floatValue() - val2.floatValue();
+            if (op instanceof FMUL) return val1.floatValue() * val2.floatValue();
+            if (op instanceof FDIV && val2.floatValue() != 0) return val1.floatValue() / val2.floatValue();
+
+            if (op instanceof DADD) return val1.doubleValue() + val2.doubleValue();
+            if (op instanceof DSUB) return val1.doubleValue() - val2.doubleValue();
+            if (op instanceof DMUL) return val1.doubleValue() * val2.doubleValue();
+            if (op instanceof DDIV && val2.doubleValue() != 0) return val1.doubleValue() / val2.doubleValue();
+
+            return null;
+        }
+
+	private Boolean compute_comparison_result(Number val1, Number val2, Instruction op){
+	    if(op instanceof IF_ICMPGT)return val1.intValue() > val2.intValue();
+	    if (op instanceof IF_ICMPLT) return val1.intValue() < val2.intValue();
+            if (op instanceof IF_ICMPEQ) return val1.intValue() == val2.intValue();
+            if (op instanceof LCMP) return val1.longValue() > val2.longValue();
+	    return null;
+	}
+
+	private Instruction get_push_instruction(Number value) {
+            if (value instanceof Integer) return new ICONST(value.intValue());
+            if (value instanceof Long) return new LCONST(value.longValue());
+            if (value instanceof Float) return new FCONST(value.floatValue());
+            if (value instanceof Double) return new DCONST(value.doubleValue());
+            return null;
 	}
 
 	
