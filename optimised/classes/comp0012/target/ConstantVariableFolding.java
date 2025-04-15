40b9d0f2a380f8870d16d0ec037238556a17f0f8
Uras <uras.asil2004@gmail.com>
2025-04-15 21:52:22 +0100

    dynamic

---
+++ optimised/classes/comp0012/target/ConstantVariableFolding.java
@@ -0,0 +1,19 @@
+package comp0012.target;
+
+public class ConstantVariableFolding {
+   public int methodOne() {
+      return 3650;
+   }
+
+   public double methodTwo() {
+      return 1.67;
+   }
+
+   public boolean methodThree() {
+      return false;
+   }
+
+   public boolean methodFour() {
+      return true;
+   }
+}
